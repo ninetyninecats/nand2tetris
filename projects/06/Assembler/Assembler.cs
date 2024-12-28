65830e1c114d9114190401b7ff3454d3df093144
ninetyninecats <catticus.bayne@proton.me>
2024-12-27 16:18:05 -0800

    Assembler and VMTranslator.

---
+++ projects/06/Assembler/Assembler.cs
@@ -0,0 +1,193 @@
+﻿public class Assembler {
+
+    static int lineno;
+    public static void Main (string[] args) {
+        if (args.Length != 1) {
+            Console.WriteLine("Usage: dotnet run file.asm");
+            return;
+        }
+        string[] lines;
+        try {
+            lines = File.ReadAllText(args[0]).Replace("\r", "").Split("\n"); 
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+        // first pass to compute label addresses
+        var instrAddr = 0;
+        var labels = new Dictionary<string, int>();
+        var vars = new Dictionary<string, int>();
+        foreach (var rawline in lines) {
+            lineno += 1;
+            var line = PreProcess(rawline);
+            if (line == null) continue;
+            if (line.StartsWith("(")) {
+                if (!line.EndsWith(")")) {
+                    Warn($"Invalid label: {line}");
+                } else {
+                    var label = line.Substring(1, line.Length - 2);
+                    try {
+                        labels.Add(label, instrAddr);
+                    } catch (ArgumentException) {
+                        Warn($"Duplicate label: {label}");
+                    }
+                }
+            } else {
+                instrAddr += 1;
+            }
+        }
+
+        var instrs = new List<string>();
+        lineno = 0;
+        foreach (var rawline in lines) {
+            lineno += 1;
+            var line = PreProcess(rawline);
+            if (line == null) continue;
+            int instr = 0;
+            if (line[0] == '@') {
+                var addr = line.Substring(1);
+                // handle numeric addresses, like @123
+                instr = ParseAddr(addr, labels, vars);
+            } else if (line.StartsWith("(")) {
+                continue;
+            } else {
+                SetBits(ref instr, 13, 3, 0b111);
+                var parts = line.Split(';');
+                var dec = parts[0].Split('=');
+                if (dec.Length == 2) {
+                    SetBits(ref instr, 3, 3, DestBits(dec[0]));
+                    SetBits(ref instr, 6, 7, CompBits(dec[1]));
+                } else {
+                    SetBits(ref instr, 6, 7, CompBits(dec[0]));
+                }
+                if (parts.Length > 1) {
+                    SetBits(ref instr, 0, 3, JumpBits(parts[1]));
+                }
+            }
+            var itext = Convert.ToString(instr, 2).PadLeft(16, '0');
+            instrs.Add(itext);
+            // Console.WriteLine(itext);
+        }
+        var filename = args[0].EndsWith(".asm") ? args[0].Substring(0, args[0].Length-4) + ".hack" : args[0] + ".hack";
+        File.WriteAllLines(filename, instrs);
+        Console.WriteLine($"Wrote {instrs.Count} instructions to '{filename}'.");
+    }
+    static void SetBits (ref int word, int offset, int bits, int value) {
+        var cmask = ((1 << bits) - 1) << offset;
+        var smask = (value << offset) & cmask;
+        word &= ~cmask;
+        word |= smask;
+    }
+    static void Warn (string message) {
+        Console.WriteLine($"Warning (line {lineno}): {message}");
+    }
+
+    static int DestBits (string dest) {
+        int bits = 0;
+        foreach (var c in dest) {
+            if (c == 'A') bits |= 0b100;
+            else if (c == 'D') bits |= 0b010;
+            else if (c == 'M') bits |= 0b001;
+            else Warn($"Unknown destination '{c}'.");
+        }
+        return bits;
+        
+    }
+    static int CompBits (string comp) {
+        switch (comp) {
+            case "0": return 0b0101010;
+            case "1": return 0b0111111;
+            case "-1": return 0b0111010;
+            case "D": return 0b0001100;
+            case "A": return 0b0110000;
+            case "M": return 0b1110000;
+            case "!D": return 0b0001101;
+            case "!A": return 0b0110001;
+            case "!M": return 0b1110001;
+            case "-D": return 0b0001111;
+            case "-A": return 0b0110011;
+            case "-M": return 0b1110011;
+            case "D+1": return 0b0011111;
+            case "A+1": return 0b0110111;
+            case "M+1": return 0b1110111;
+            case "D-1": return 0b0001110;
+            case "A-1": return 0b0110010;
+            case "M-1": return 0b1110010;
+            case "D+A":
+            case "A+D": return 0b0000010;
+            case "D+M":
+            case "M+D": return 0b1000010;
+            case "D-A": return 0b0010011;
+            case "A-D": return 0b0000111;
+            case "D-M": return 0b1010011;
+            case "M-D": return 0b1000111;
+            case "D&A":
+            case "A&D": return 0b0000000;
+            case "D&M":
+            case "M&D": return 0b1000000;
+            case "D|A":
+            case "A|D": return 0b0010101;
+            case "D|M":
+            case "M|D": return 0b1010101;
+            default: 
+                Warn($"Unknown comp: '{comp}'");
+                return 0;
+        }
+    }
+    static int JumpBits(string jump) {
+        switch (jump) {
+            case "JGT": return 0b001; 
+            case "JEQ": return 0b010; 
+            case "JGE": return 0b011; 
+            case "JLT": return 0b100; 
+            case "JNE": return 0b101; 
+            case "JLE": return 0b110; 
+            case "JMP": return 0b111; 
+            default: 
+                Warn($"Unknown jump: '{jump}'");
+                return 0;
+        }
+    }
+    static string? PreProcess (string line) {
+        var cidx = line.IndexOf("//");
+        if (cidx >= 0) {
+            line = line.Substring(0, cidx);
+        }
+        line = line.Replace(" ", "");
+        return line.Length > 0 ? line : null;
+    }
+
+    static int ParseAddr (string addr, Dictionary<string, int> labels, Dictionary<string, int> vars) {
+        try {
+            return Int16.Parse(addr);
+        } catch (OverflowException) {
+            Warn($"Address too large: {addr}");
+        }
+        catch (FormatException) {} // fall through
+
+        // handle I/O
+        if (addr == "SCREEN") return 16384;
+        if (addr == "KBD") return 16384 + 8192;
+
+        // handle @R0, @R1, etc.
+        if (addr.StartsWith("R")) {
+            try {
+                var reg = Int16.Parse(addr.Substring(1));
+                if (reg > 15) {
+                    Warn($"Registers should only be 0-15: {addr}");
+                }
+                return reg;
+            } catch (FormatException) {} // fall through
+        }
+
+        // handle labels, like @EXIT, etc.
+        if (labels.TryGetValue(addr, out var instrAddr)) return instrAddr;
+
+        // otherwise it must be a variable
+        if (vars.TryGetValue(addr, out var varAddr)) return varAddr;
+        varAddr = 16 + vars.Count;
+        vars.Add(addr, varAddr);
+        return varAddr;
+    }
+}
---
+++ projects/07/VMTranslator/VMTranslator.cs
@@ -0,0 +1,236 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Principal;
+using System.Linq;
+
+public class VMTranslator {
+
+    static int lineno;
+    static List<string> instrs = new List<string>();
+    static int pc;
+    static string currentFile = "";
+    static string currentModule = "";
+    static string currentFunction = "";
+
+    public static void Main (string[] args) {
+        if (args.Length < 2) {
+            Console.WriteLine("Usage: dotnet run output.asm file.vm [file.vm ...]");
+            return;
+        }
+
+        // emit the bootstrap instructions
+        Emit("@256", "D = A", "@0", "M = D");
+        EmitCall("Sys.init", 0);
+
+        foreach (var file in args.Skip(1)) {
+            Assemble(file);
+        }
+
+        File.WriteAllLines(args[0], instrs);
+        Console.WriteLine($"Wrote {instrs.Count} instructions to '{args[0]}'.");
+    }
+
+    private static void Assemble (string file) {
+        currentFile = file;
+        var lastSlash = currentFile.LastIndexOf(Path.DirectorySeparatorChar);
+        currentModule = currentFile.Substring(lastSlash+1);
+        if (currentFile.EndsWith(".vm")) {
+            currentModule = currentModule.Substring(0, currentModule.Length-3);
+        }
+
+        var lines = File.ReadAllText(file).Replace("\r", "").Split("\n");
+        foreach (var rawline in lines) {
+            lineno += 1;
+            var line = PreProcess(rawline);
+            if (line == null) continue;
+            var parts = line.Split(" ");
+            switch (parts[0]) {
+            case "push":
+                EmitPush(parts[1], Convert.ToInt32(parts[2]));
+                break;
+            case "pop":
+                EmitPop(parts[1], Convert.ToInt32(parts[2]));
+                break;
+            case "add": case "sub": case "and": case "or":
+                EmitBinOp(parts[0]);
+                break;
+            case "not": case "neg":
+                EmitUnOp(parts[0]);
+                break;
+            case "eq": case "lt": case "gt":
+                EmitBinCmp(parts[0]);
+                break;
+            case "label":
+                Emit($"({currentFunction}.{parts[1]})");
+                break;
+            case "goto":
+                Emit($"@{currentFunction}.{parts[1]}", "0;JMP");
+                break;
+            case "if-goto":
+                EmitPopIntoD();
+                Emit($"@{currentFunction}.{parts[1]}", "D;JNE");
+                break;
+            case "call":
+                EmitCall(parts[1], Convert.ToInt32(parts[2]));
+                break;
+            case "function":
+                currentFunction = parts[1];
+                EmitFunction(parts[1], Convert.ToInt32(parts[2]));
+                break;
+            case "return":
+                EmitReturn();
+                break;
+            default:
+                Warn($"Unhandled instruction: {line}");
+                break;
+            }
+        }
+    }
+
+    static void Warn (string message) {
+        Console.WriteLine($"Warning (line {lineno}): {message}");
+    }
+
+    static string? PreProcess (string line) {
+        var cidx = line.IndexOf("//");
+        if (cidx >= 0) {
+            line = line.Substring(0, cidx);
+        }
+        // line = line.Replace(" ", "");
+        line = line.Trim();
+        return line.Length > 0 ? line : null;
+    }
+
+    static void EmitPush(string segment, int offset) {
+        switch (segment) {
+        case "constant":
+            Emit($"@{offset}", "D = A");
+            break;
+
+        case "local":
+        case "argument":
+        case "this":
+        case "that":
+            var baseAddr = segment == "local" ? "@1" :
+                segment == "argument" ? "@2" :
+                segment == "this" ? "@3" :
+                /* segment == that */ "@4";
+            Emit(baseAddr, "D = M", $"@{offset}", "A = D + A", "D = M");
+            break;
+
+        case "static":
+            Emit($"@{currentModule}.{offset}", "D = M");
+            break;
+        case "temp":
+        case "pointer":
+            var baseOffset = segment == "temp" ? 5 : 3;
+            Emit($"@{offset + baseOffset}", "D = M");
+            break;
+        }
+        EmitPushFromD();
+    }
+
+    static void EmitPop(string segment, int offset) {
+        switch (segment) {
+        case "local":
+        case "argument":
+        case "this":
+        case "that":
+            var baseAddr = segment == "local" ? "@1" :
+                segment == "argument" ? "@2" :
+                segment == "this" ? "@3" :
+                /* segment == that */ "@4";
+            Emit(baseAddr, "D = M", $"@{offset}", "D = D + A", "@R13", "M = D");
+            EmitPopIntoD();
+            Emit("@R13", "A = M", "M = D");
+            break;
+
+        case "static":
+            Emit($"@{currentModule}.{offset}", "M = D");
+            break;
+        case "temp":
+        case "pointer":
+            EmitPopIntoD();
+            var baseOffset = segment == "temp" ? 5 : 3;
+            Emit($"@{offset + baseOffset}", "M = D");
+            break;
+        }
+    }
+
+    static void EmitBinOp(string op) {
+        EmitPopIntoD();
+        Emit("A = M - 1");
+        switch (op) {
+        case "add": Emit("M = M + D"); break;
+        case "sub": Emit("M = M - D"); break;
+        case "and": Emit("M = M & D"); break;
+        case "or": Emit("M = M | D"); break;
+        }
+    }
+    static void EmitUnOp(string op) {
+        Emit("@0", "A = M - 1");
+        if (op == "neg") Emit("M = -M");
+        else if (op == "not") Emit("M = !M");
+        else Warn($"Unexpected Unary operator {op}");
+    }
+    static void EmitBinCmp(string cmp) {
+        var jmp = "J" + cmp.ToUpper();
+        EmitPopIntoD();
+        Emit("A = M - 1", "D = M - D",
+             $"@IFEQ{lineno}", $"D;{jmp}", "D = 0", $"@WB{lineno}", "0;JMP",
+             $"(IFEQ{lineno})", "D = -1",
+             $"(WB{lineno})", "@0", "A = M - 1", "M = D");
+    }
+    static int EmitPopIntoD() {
+        return Emit("@0", "A = M - 1", "D = M", "@0", "M = M - 1");
+    }
+    static int EmitPushFromD() {
+        return Emit("@0", "A = M", "M = D", "@0", "M = M + 1");
+    }
+    static void EmitCall(string fn, int args) {
+        Emit($"// EmitCall {fn} {args}");
+        var emitRA = instrs.Count;
+        Emit("@TODO", "D = A");
+        EmitPushFromD();
+        Emit("@1", "D = M");
+        EmitPushFromD();
+        Emit("@2", "D = M");
+        EmitPushFromD();
+        Emit("@3", "D = M");
+        EmitPushFromD();
+        Emit("@4", "D = M");
+        EmitPushFromD();
+        Emit("@0", "D = M", "@1", "M = D", 
+             $"@{args + 5}", "D = D - A", "@2", "M = D");
+        Emit($"@{fn}", "0;JMP");
+        instrs[emitRA] = $"@{pc}";
+    }
+
+    private static void EmitFunction(string fn, int locals) {
+        Emit($"({fn})");
+        for (var ii = 0 ; ii < locals; ii ++) {
+            EmitPush("constant", 0);
+        }
+    }
+
+    private static void EmitReturn() {
+        Emit("// EmitReturn");
+        Emit("@1", "D = M", "@5", "A = D - A", "D = M", "@R14", "M = D");
+        EmitPop("argument", 0);
+        Emit("@2", "D = M", "@0", "M = D + 1");
+        for (var ii = 1; ii <= 4; ii ++) {
+            Emit("@1", "D = M", $"@{ii}", "A = D - A", "D = M", 
+                 $"@{5 - ii}", "M = D");
+        }
+        Emit("@R14", "A = M", "0;JMP");
+    }
+
+    static int Emit(params string[] instrs) {
+        //foreach (var instr in instrs) Console.WriteLine(instr);
+        foreach (var instr in instrs) {
+            VMTranslator.instrs.Add(instr);
+            if (!instr.StartsWith("(") && 
+                !instr.StartsWith("//")) pc += 1;
+        }
+        return instrs.Length;
+    }
+}
